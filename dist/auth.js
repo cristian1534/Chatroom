61e3600b8506eac739365978c8c20b86233b2e07
cristian1534 <cmachuca32@gmail.com>
2023-07-18 13:56:28 +0000

    Updating with motions Project.

--- dist/auth.js
+++ dist/auth.js
@@ -1,6 +1,6 @@
-let token = localStorage.getItem("token");
+// let token = localStorage.getItem("token");
 
-if (!token) {
-  window.location.href =
-    "https://e-learning-3ewwv8v2o-cristian1534.vercel.app/auth/login";
-}
+// if (!token) {
+//   window.location.href =
+//     "https://e-learning-3ewwv8v2o-cristian1534.vercel.app/auth/login";
+// }
